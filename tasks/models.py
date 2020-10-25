f66b062ff91d202bcc2f29acd18807415d16ed5d
Aswin TR <aswintr252@gmail.com>
2020-10-25 12:01:54 +0530

    created the view to create a task and list them

--- tasks/models.py
+++ tasks/models.py
@@ -7,4 +7,7 @@ class Tasks(models.Model):
     title = models.CharField(max_length=200)
     complete = models.BooleanField(default=False)
     created = models.DateTimeField(auto_now_add=True)
-    
+
+
+    def __str__(self):
+        return self.title
--- tasks/views.py
+++ tasks/views.py
@@ -1,6 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from .models import Tasks
+from .forms import TasksForm
 
 # Create your views here.
 def index(request):
-    return HttpResponse("hello world")
+    tasks = Tasks.objects.all()
+    form = TasksForm()
+
+    if request.method == "POST":
+        form = TasksForm(request.POST)
+        if form.is_valid():
+            form.save()
+        return redirect('/')
+    
+    context = {'tasks': tasks, 'form': form}
+    return render(request, 'tasks/tasks.html', context)
